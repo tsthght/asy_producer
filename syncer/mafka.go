ba19c180d85502891a09deb7a069118342712020
tsthght <tsthght@163.com>
2020-05-05 16:09:06 +0800

    refine mafka

--- syncer/mafka.go
+++ syncer/mafka.go
@@ -6,8 +6,14 @@ package syncer
 //#include "libcommon.h"
 import "C"
 import (
+	"errors"
+
+	"github.com/pingcap/tidb-binlog/drainer/loopbacksync"
+	"github.com/pingcap/tidb-binlog/drainer/relay"
 	dsync "github.com/pingcap/tidb-binlog/drainer/sync"
 	"github.com/tsthght/syncer/orderlist"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/pingcap/tidb-binlog/drainer/translator"
 )
 
 type MafkaSyncer struct {
@@ -15,6 +21,44 @@ type MafkaSyncer struct {
 	configFile      string
 }
 
+func NewMafkaSyncer(
+	cfg *dsync.DBConfig,
+	cfgFile string,
+	tableInfoGetter translator.TableInfoGetter,
+	worker int,
+	batchSize int,
+	queryHistogramVec *prometheus.HistogramVec,
+	sqlMode *string,
+	destDBType string,
+	relayer relay.Relayer,
+	info *loopbacksync.LoopBackSync,
+	enableDispatch bool,
+	enableCausility bool) (dsyncer dsync.Syncer, err error) {
+	if cfgFile == "" {
+		return nil, errors.New("config file name is empty")
+	}
+
+	executor := &MafkaSyncer{}
+
+	return executor, nil
+}
+
 func (ms *MafkaSyncer) Sync(item *dsync.Item) error {
 	return nil
+}
+
+func (ms *MafkaSyncer) Close() error {
+	return nil
+}
+
+func (ms *MafkaSyncer) Successes() <-chan *dsync.Item {
+	return nil
+}
+
+func (ms *MafkaSyncer) Error() <-chan error {
+	return nil
+}
+
+func (ms *MafkaSyncer) SetSafeMode(mode bool) bool {
+	return false
 }
