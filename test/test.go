4390db7060d4b64b7ea6419f4179d85e370cf7fc
tsthght <tsthght@163.com>
2020-05-05 11:52:39 +0800

    add log

--- test/test.go
+++ test/test.go
@@ -5,12 +5,16 @@ package main
 //
 //#include "libcommon.h"
 import "C"
-import "fmt"
+import (
+	"fmt"
+	"github.com/pingcap/log"
+)
 
 func main() {
 	for i:=0; i< 10; i++ {
 		C.AsyncMessage(C.CString("hello golang"), 21)
 		p := int64(C.GetLatestApplyTime())
+		log.Info("test")
 		fmt.Printf("## %d\n", p)
 	}
 }
