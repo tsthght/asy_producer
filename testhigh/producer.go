9eac9914c9452bd9396e72c8c6c5e7a5b3b0ac3a
tsthght <tsthght@163.com>
2020-05-06 16:15:26 +0800

    add waitthrehold

--- testhigh/producer.go
+++ testhigh/producer.go
@@ -121,7 +121,7 @@ func (ms *MafkaSyncer) Run () {
 				if /*ms.toBeAckCommitTS.Size() > 0 && */time.Now().Unix() - tss > ms.maxWaitThreshold {
 					//err := errors.New(fmt.Sprintf("fail to push msg to kafka after %v, check if kafka is up and working", ms.maxWaitThreshold))
 					//ms.SetErr(err)
-					close(ms.shutdown)
+					ms.Close()
 				}
 				ms.toBeAckCommitTSMu.Unlock()
 			case <-ms.shutdown:
