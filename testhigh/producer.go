1052f4598152ed6f6e84c9b010da692ac86137e0
tsthght <tsthght@163.com>
2020-05-06 09:46:58 +0800

    add testhigh

---
+++ testhigh/producer.go
@@ -0,0 +1,131 @@
+package main
+
+//#cgo CFLAGS: -I ../common
+//#cgo LDFLAGS: -L ../common  -Wl,-rpath=/usr/local/lib -lcommon
+//
+//#include "libcommon.h"
+import "C"
+import (
+	"container/list"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+	"errors"
+
+	"github.com/pingcap/log"
+	"github.com/tsthght/syncer/orderlist"
+	"go.uber.org/zap"
+)
+
+type MafkaSyncer struct {
+	toBeAckCommitTSMu      sync.Mutex
+	toBeAckCommitTS *orderlist.MapList
+	shutdown chan struct{}
+}
+
+type Item struct {
+	data string
+	ts int64
+}
+
+func (it *Item) GetKey() int64 {
+	return it.ts
+}
+
+func NewMafkaSyncer(cfgFile string) (*MafkaSyncer, error){
+	if cfgFile == "" {
+		return nil, errors.New("config file name is empty")
+	}
+	ret := C.InitProducerOnce(C.CString(cfgFile))
+	if len(C.GoString(ret)) == 0 {
+		return nil, errors.New("init producer error")
+	}
+
+	executor := &MafkaSyncer{}
+	executor.shutdown = make(chan struct{})
+	executor.toBeAckCommitTS = orderlist.NewMapList()
+	return executor, nil
+}
+
+func (ms *MafkaSyncer) Sync(item *Item) error {
+	//do sth translator
+	C.AsyncMessage(C.CString(string(item.data)), C.long(item.ts))
+	ms.toBeAckCommitTSMu.Lock()
+	ms.toBeAckCommitTS.Push(item)
+	ms.toBeAckCommitTSMu.Unlock()
+	return nil
+}
+
+func (ms *MafkaSyncer) Close() {
+	C.CloseProducer()
+}
+
+func (ms *MafkaSyncer) SetSafeMode(mode bool) bool {
+	return false
+}
+
+func (ms *MafkaSyncer) Run () {
+	var wg sync.WaitGroup
+
+	// handle successes from producer
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		ts := int64(C.GetLatestApplyTime())
+		ms.toBeAckCommitTSMu.Lock()
+		var next *list.Element
+		for elem := ms.toBeAckCommitTS.GetDataList().Front(); elem != nil; elem = next {
+			if elem.Value.(orderlist.Keyer).GetKey() <= ts {
+				next = elem.Next()
+				//ms.success <- elem.Value.(*Item)
+				fmt.Printf("##### %v, %v", elem.Value.(*Item).data, elem.Value.(*Item).ts)
+				ms.toBeAckCommitTS.Remove(elem.Value.(orderlist.Keyer))
+			} else {
+				break
+			}
+		}
+		ms.toBeAckCommitTSMu.Unlock()
+
+		time.Sleep(1 * time.Second)
+	}()
+
+	for {
+		select {
+		case <-ms.shutdown:
+			C.CloseProducer()
+			//ms.SetErr(nil)
+
+			wg.Wait()
+			return
+		}
+	}
+}
+
+func main() {
+	cfg := &log.Config{Level: "debug", File: log.FileLogConfig{Filename:"./testhigh.log"}, DisableTimestamp: true}
+	lg, _globalP, _ := log.InitLogger(cfg)
+	lg = lg.WithOptions(zap.AddStacktrace(zap.DPanicLevel))
+	log.ReplaceGlobals(lg, _globalP)
+
+	producer, err := NewMafkaSyncer("../mafka/mafka.toml")
+	if err != nil {
+		fmt.Printf("%s\n", err.Error())
+		os.Exit(1)
+	}
+	defer producer.Close()
+
+	//generator data
+	go func() {
+		time.Sleep(5 * time.Second)
+		for i:=1;i > 0; i++ {
+			it := Item{"hello mafka", int64(i)}
+			producer.Sync(&it)
+			time.Sleep(500 * time.Millisecond)
+		}
+	}()
+
+	producer.Run()
+
+}
